66df6f165a372eb41a8f141d62d5ca339b514ec4
icydwei <robpaik@gmail.com>
2019-12-13 15:38:35 -0500

    brute force solution

--- lib/sum_square_difference.rb
+++ lib/sum_square_difference.rb
@@ -1,5 +1,19 @@
 # Implement your procedural solution here!
 def sum_square_difference(number)
-  
+  sum = 0
+  for i in 1..number do
+    sum += i**2
+  end
+
+  squared = 0
+  for i in 1..number do
+    squared += i
+  end
+
+  squared = squared**2
+  puts squared-sum
 
 end
+
+
+sum_square_difference(5)
